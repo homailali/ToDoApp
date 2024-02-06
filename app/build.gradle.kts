962e1ecceae1b1c5a063d6439eaf45c24af4412f
homailali <138436251+homailali@users.noreply.github.com>
2024-02-06 10:33:04 +0500

    Making some improvements

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "life.homail.todoapp"
-        minSdk = 24
+        minSdk = 21
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -36,7 +36,6 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.cardview:cardview:1.0.0")
     testImplementation("junit:junit:4.13.2")
--- app/src/main/java/life/homail/todoapp/Model/TimeAndTask.java
+++ app/src/main/java/life/homail/todoapp/Model/TimeAndTask.java
@@ -1,21 +1,35 @@
 package life.homail.todoapp.Model;
 
 public class TimeAndTask {
+    // Fields
     private String task;
     private String date;
     private String time;
-
-
-
-
-
-    public void setTask(String task){
-        this.task=task;
+    // Constructor
+    public TimeAndTask(String task, String date, String time) {
+        this.task = task;
+        this.date = date;
+        this.time = time;
     }
-    public void setTime(String time){
-        this.time=time;
+    // Methods
+    // Setters
+    public void setTask(String task) {
+        this.task = task;
     }
-    public void setDate(String date){
-        this.date=date;
+    public void setDate(String date) {
+        this.date = date;
+    }
+    public void setTime(String time) {
+        this.time = time;
+    }
+    // Getters
+    public String getTask() {
+        return this.task;
+    }
+    public String getDate() {
+        return this.date;
+    }
+    public String getTime() {
+        return this.time;
     }
 }
